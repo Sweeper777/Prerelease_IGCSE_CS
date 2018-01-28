81b15a53e3b6241f9218bc976f993fa4c2abcb99
Sweeper777 <sumulang@gmail.com>
2018-01-28 08:34:53 +0000

    added Choice class

---
+++ Prerelease_IGCSE_CS/Choice.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Prerelease_IGCSE_CS
+{
+    public class Choice
+    {
+        public string ComponentName { get; }
+        public string Name { get; }
+        public int Price { get; }
+
+    }
+}
