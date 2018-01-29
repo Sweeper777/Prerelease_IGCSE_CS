13e0e8de5c69580624578653660ee5198384dfe8
Sweeper777 <sumulang@gmail.com>
2018-01-29 17:01:16 +0000

    added Stock class

---
+++ Prerelease_IGCSE_CS/Stock.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Prerelease_IGCSE_CS
+{
+    public static class Stock
+    {
+        public static readonly Dictionary<Choice, int> AllStock;
+    }
+}
