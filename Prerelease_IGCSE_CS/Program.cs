78d5ae765e35ae37cf43184b9cc5e930b238f9f0
Sweeper777 <sumulang@gmail.com>
2018-01-29 16:58:09 +0000

    added GetEstimate

--- Prerelease_IGCSE_CS/Program.cs
+++ Prerelease_IGCSE_CS/Program.cs
@@ -8,6 +8,23 @@ namespace Prerelease_IGCSE_CS
     {
         public static void Main(string[] args)
         {
+        static Estimate GetEstimate()
+        {
+            var choices = new List<Choice>();
+            foreach (var component in Choice.ChoiceDictionary) 
+            {
+                Console.WriteLine($"Please select a {component.Key}:");
+                int i = 1;
+                foreach (var choice in component) 
+                {
+                    Console.WriteLine($"{i}. {choice.Name} ${choice.Price}");
+                    i++;
+                }
+                var chosen = component.ElementAt(Convert.ToInt32(Console.ReadLine()) - 1);
+                choices.Add(chosen);
+            }
+            return new Estimate(choices);
+        }
         }
     }
 }
