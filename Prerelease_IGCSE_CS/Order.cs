6003393158214e8616177e753898e530aaba8b39
Sweeper777 <sumulang@gmail.com>
2018-01-30 07:09:44 +0000

    added Order constructor

--- Prerelease_IGCSE_CS/Order.cs
+++ Prerelease_IGCSE_CS/Order.cs
@@ -7,5 +7,13 @@ namespace Prerelease_IGCSE_CS
         public string CustomerName { get; }
         public DateTime Date { get; } = DateTime.Today;
         public Estimate EstimateDetails { get; }
+
+        public Order(string customerName, Estimate estimateDetails)
+        {
+            CustomerName = customerName;
+            EstimateDetails = estimateDetails;
+            AllOrders.Add(this);
+        }
+
     }
 }
