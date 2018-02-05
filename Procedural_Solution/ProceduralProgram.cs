9cce8060ef1a3ed0b9ae14f4b737d1198b80a763
Sweeper777 <sumulang@gmail.com>
2018-02-05 18:49:02 +0000

    added RAM choices and prices

--- Procedural_Solution/ProceduralProgram.cs
+++ Procedural_Solution/ProceduralProgram.cs
@@ -9,5 +9,8 @@ namespace Procedural_Solution
         static IReadOnlyList<string> processorChoices = new List<string> { "p3", "p5", "p7" }.AsReadOnly();
         static IReadOnlyList<int> processorPrices = new List<int> { 100, 120, 200 }.AsReadOnly();
 
+        static IReadOnlyList<string> ramChoices = new List<string> { "16GB", "32GB" }.AsReadOnly();
+        static IReadOnlyList<int> ramPrices = new List<int> { 75, 150 }.AsReadOnly();
+
     }
 }
