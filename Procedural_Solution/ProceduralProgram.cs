99fbfc1627e823274a219aceb91b1dffae5dcc75
Sweeper777 <sumulang@gmail.com>
2018-02-05 18:48:49 +0000

    added processor choices and prices

--- Procedural_Solution/ProceduralProgram.cs
+++ Procedural_Solution/ProceduralProgram.cs
@@ -6,5 +6,8 @@ namespace Procedural_Solution
     class MainClass
     {
         
+        static IReadOnlyList<string> processorChoices = new List<string> { "p3", "p5", "p7" }.AsReadOnly();
+        static IReadOnlyList<int> processorPrices = new List<int> { 100, 120, 200 }.AsReadOnly();
+
     }
 }
